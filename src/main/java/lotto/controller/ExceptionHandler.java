046f97aa6236e52d781b538a0b8be8e12796727b
june-777 <wlwhswnsrl96@gmail.com>
2023-11-23 15:46:50 +0900

    feat(ExceptionHandler): 예외가 발생하지 않을 때 까지 무한루프를 진행하는 예외 처리 핸들러

---
+++ src/main/java/lotto/controller/ExceptionHandler.java
@@ -0,0 +1,15 @@
+package lotto.controller;
+
+import java.util.function.Supplier;
+
+public class ExceptionHandler {
+    public static <T> T handle(final Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}

909fe1db59598ea3c3d8046b18af6430e1afafd8
june-777 <wlwhswnsrl96@gmail.com>
2023-11-23 15:49:28 +0900

    feat(Lotto): 로또에 속한 번호가 중복인지 검증하는 기능

---
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -0,0 +1,33 @@
+package lotto.domain.lotto;
+
+import java.util.HashSet;
+import java.util.List;
+import lotto.exception.LottoException;
+
+public class Lotto {
+    private static final int REQUIRED_SIZE = 6;
+    private final List<LottoNumber> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = toLottoNumbers(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateDuplicate(numbers);
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        HashSet<Integer> uniqueNumbers = new HashSet<>(numbers);
+
+        if (uniqueNumbers.size() != numbers.size()) {
+            LottoException.LOTTO_DUPLICATE.make();
+        }
+    }
+
+    private List<LottoNumber> toLottoNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::new)
+                .toList();
+    }
+}

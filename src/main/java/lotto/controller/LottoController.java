b1e661707e64f971ca28a0743b75a570aa57f893
june-777 <wlwhswnsrl96@gmail.com>
2023-11-23 22:21:18 +0900

    feat(LottoController): 프로세스에 맞게 로직 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,26 +1,52 @@
 package lotto.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
+import lotto.domain.PlayerResult;
 import lotto.domain.PurchaseAmount;
 import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoNumber;
+import lotto.domain.lotto.PlayerLotto;
+import lotto.domain.lotto.WinningInformation;
+import lotto.dto.mapper.PurchaseLottoViewMapper;
 import lotto.service.LottoOffice;
+import lotto.service.YieldCalculator;
 import lotto.view.InputView;
 import lotto.view.PurchaseLottoView;
+import lotto.view.StatisticsView;
 
 public class LottoController {
 
     private final InputView inputView;
     private final LottoOffice lottoOffice;
+    private final YieldCalculator yieldCalculator;
 
-    public LottoController(InputView inputView, LottoOffice lottoOffice) {
+    public LottoController(InputView inputView, LottoOffice lottoOffice, YieldCalculator yieldCalculator) {
         this.inputView = inputView;
         this.lottoOffice = lottoOffice;
+        this.yieldCalculator = yieldCalculator;
     }
 
     public void process() {
-        PurchaseAmount purchaseAmount = setPurchaseAmount();
-        List<Lotto> lottos = lottoOffice.issueLottos(purchaseAmount);
-        PurchaseLottoView.print(mapFrom(lottos));
+        final PurchaseAmount purchaseAmount = setPurchaseAmount();
+        final List<Lotto> lottos = lottoOffice.issueLottos(purchaseAmount);
+        final PlayerLotto playerLotto = new PlayerLotto(lottos);
+
+        renderingPurchaseLotto(lottos);
+
+        final WinningInformation winningInformation = publishWinningInformation();
+        final PlayerResult playerResult = playerLotto.calculateWinningResult(winningInformation);
+        final BigDecimal yield = yieldCalculator.calculate(playerResult, purchaseAmount);
+        renderingStatistics(playerResult, yield);
+    }
+
+    private WinningInformation publishWinningInformation() {
+        Lotto lotto = setWinningNumber();
+        return setWinningInformation(lotto);
+    }
+
+    private void renderingPurchaseLotto(List<Lotto> lottos) {
+        PurchaseLottoView.print(PurchaseLottoViewMapper.mapFrom(lottos));
     }
 
     private PurchaseAmount setPurchaseAmount() {
@@ -30,9 +56,22 @@ public class LottoController {
         });
     }
 
-    private List<List<Integer>> mapFrom(List<Lotto> lottos) {
-        return lottos.stream()
-                .map(Lotto::calculateNumbers)
-                .toList();
+    private Lotto setWinningNumber() {
+        return ExceptionHandler.handle(() -> {
+            List<Integer> winningNumbersInput = inputView.readWinningNumbers();
+            return new Lotto(winningNumbersInput);
+        });
+    }
+
+    private WinningInformation setWinningInformation(Lotto winningNumbers) {
+        return ExceptionHandler.handle(() -> {
+            int bonusNumberInput = inputView.readBonusNumber();
+            LottoNumber bonusNumber = new LottoNumber(bonusNumberInput);
+            return new WinningInformation(winningNumbers, bonusNumber);
+        });
+    }
+
+    private static void renderingStatistics(PlayerResult playerResult, BigDecimal yield) {
+        StatisticsView.print(playerResult.getRankMatchCounts(), yield);
     }
 }

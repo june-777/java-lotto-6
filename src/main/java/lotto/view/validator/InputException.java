5196b655a5e518a53e44e157cce1db1574e9bdfb
june-777 <wlwhswnsrl96@gmail.com>
2023-11-23 14:12:49 +0900

    refactor(InputException): 당첨 번호 입력 관련 예외 메시지 추가

--- src/main/java/lotto/view/validator/InputException.java
+++ src/main/java/lotto/view/validator/InputException.java
@@ -2,8 +2,9 @@ package lotto.view.validator;
 
 public enum InputException {
     BLANK("공백 입력은 안됩니다."),
-    EXCEED_PURCHASE_AMOUNT_LENGTH("로또 구입 금액 입력은 9자를 초과할 수 없습니다."),
-    NOT_NUMERIC("숫자가 아닌 입력이 존재합니다.");
+    EXCEED_INPUT_LENGTH("로또 구입 금액 입력은 9자를 초과할 수 없습니다."),
+    NOT_NUMERIC("숫자가 아닌 입력이 존재합니다."),
+    INVALID_WINNING_NUMBER_FORM("콤마는 입력 처음과 끝에 존재해선 안됩니다.");
 
     private String PREFIX = "[ERROR] ";
     private final String message;

ce26afe4f8be0a9f1c5138c4dcc7f34957700d4a
june-777 <wlwhswnsrl96@gmail.com>
2023-11-23 22:43:26 +0900

    feat(WinningInformation): 당첨 정보를 나타내는 클래스

---
+++ src/main/java/lotto/domain/lotto/WinningInformation.java
@@ -0,0 +1,28 @@
+package lotto.domain.lotto;
+
+import lotto.exception.LottoException;
+
+public class WinningInformation {
+    private final Lotto winningNumbers;
+    private final LottoNumber bonusNumber;
+
+    public WinningInformation(Lotto winningNumbers, LottoNumber bonusNumber) {
+        validateDuplicate(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateDuplicate(Lotto winningNumbers, LottoNumber bonusNumber) {
+        if (winningNumbers.isContains(bonusNumber)) {
+            LottoException.BONUS_NUMBER_DUPLICATE.make();
+        }
+    }
+
+    public Lotto getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public LottoNumber getBonusNumber() {
+        return bonusNumber;
+    }
+}

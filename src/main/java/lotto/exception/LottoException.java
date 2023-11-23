7aafa7233b296c2f0aa86c758a3cd81ff89b5686
june-777 <wlwhswnsrl96@gmail.com>
2023-11-23 15:50:44 +0900

    feat(LottoException): 로또 관련 예외를 나타내는 Enum

---
+++ src/main/java/lotto/exception/LottoException.java
@@ -0,0 +1,22 @@
+package lotto.exception;
+
+public enum LottoException {
+    LOTTO_NUMBER_OUT_OF_RANGE("로또 숫자는 1 ~ 45 범위여야 합니다."),
+    LOTTO_DUPLICATE("로또는 중복되지 않는 숫자로 이루어져야 합니다."),
+    LOTTO_OUT_OF_SIZE("로또는 6개의 숫자로 이루어져야 합니다.");
+
+    private String PREFIX = "[ERROR] ";
+    private final String message;
+
+    LottoException(String message) {
+        this.message = message;
+    }
+
+    public void make() {
+        throw new IllegalArgumentException(PREFIX + message);
+    }
+
+    public String getMessage() {
+        return PREFIX + message;
+    }
+}

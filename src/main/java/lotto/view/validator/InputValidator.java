e0221e9397515e61f10dd7374aef42cd6a1d9755
june-777 <wlwhswnsrl96@gmail.com>
2023-11-23 14:11:28 +0900

    feat(InputValidator): 당첨 번호 입력을 검증하는 기능

--- src/main/java/lotto/view/validator/InputValidator.java
+++ src/main/java/lotto/view/validator/InputValidator.java
@@ -1,13 +1,24 @@
 package lotto.view.validator;
 
+import java.util.Arrays;
+
 public class InputValidator {
 
     private static final int PURCHASE_AMOUNT_LENGTH = 9;
+    private static final int WINNING_NUMBERS_LENGTH = 17;
+    private static final char SEPERATOR = ',';
 
     public static void validatePurchaseAmount(String purchaseAmount) {
         validateBlank(purchaseAmount);
-        validateMaxLength(purchaseAmount);
+        validateMaxLength(purchaseAmount, PURCHASE_AMOUNT_LENGTH);
         validateNumeric(purchaseAmount);
+    }
+
+    public static void validateWinningNumbers(String winningNumbers) {
+        validateBlank(winningNumbers);
+        validateMaxLength(winningNumbers, WINNING_NUMBERS_LENGTH);
+        validateWinningNumbersInputForm(winningNumbers);
+        validateAllNumeric(winningNumbers);
     }
 
     private static void validateBlank(String target) {
@@ -16,9 +27,9 @@ public class InputValidator {
         }
     }
 
-    private static void validateMaxLength(String target) {
-        if (target.length() > PURCHASE_AMOUNT_LENGTH) {
-            InputException.EXCEED_PURCHASE_AMOUNT_LENGTH.make();
+    private static void validateMaxLength(String target, int limitLength) {
+        if (target.length() > limitLength) {
+            InputException.EXCEED_INPUT_LENGTH.make();
         }
     }
 
@@ -34,6 +45,29 @@ public class InputValidator {
             return true;
         } catch (NumberFormatException e) {
             return false;
+        }
+    }
+
+    private static void validateWinningNumbersInputForm(String target) {
+        if (isFirstCharacterIsSeperator(target) || isLastCharacterIsSeperator(target)) {
+            InputException.INVALID_WINNING_NUMBER_FORM.make();
+        }
+    }
+
+    private static boolean isFirstCharacterIsSeperator(String target) {
+        return target.charAt(0) == SEPERATOR;
+    }
+
+    private static boolean isLastCharacterIsSeperator(String target) {
+        return target.charAt(target.length() - 1) == SEPERATOR;
+    }
+
+    private static void validateAllNumeric(String target) {
+        boolean notNumericExist = Arrays.stream(target.split(","))
+                .anyMatch(InputValidator::isNumeric);
+
+        if (notNumericExist) {
+            InputException.NOT_NUMERIC.make();
         }
     }
 }
